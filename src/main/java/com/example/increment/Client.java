51be21ef0b240745d7a11baa35560e73506c59e9
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-12-04 10:28:39 +0300

    interfeicik

--- src/main/java/com/example/increment/Client.java
+++ src/main/java/com/example/increment/Client.java
@@ -1,8 +1,12 @@
 package com.example.increment;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,28 +14,25 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 public class Client extends Application {
     private int counter = 0;
+    private Socket socket;
     private Label counterLabel;
-    String serverIP = "10.0.0.178"; // Replace with the IP address of your server
+    private String serverIP = "192.168.1.119"; // Replace with the IP address of your server
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view2.fxml"));
-        VBox root = loader.load();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Client");
-        primaryStage.show();
+    public void start(Stage primaryStage) throws IOException {  
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view2.fxml"));
+    VBox root = loader.load();
+    Scene scene = new Scene(root);
+    scene.getStylesheets().add(getClass().getResource("dark_theme.css").toExternalForm());
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Client");
+    primaryStage.show();
 
         // Bind counterLabel from FXML to the counterLabel field in the controller
         counterLabel = (Label) loader.getNamespace().get("lableid");
@@ -43,21 +44,25 @@ public class Client extends Application {
             sendCounterValueToServer();
         });
 
-        new Thread(() -> {
-            try {
-                Socket socket = new Socket(serverIP, 41242);
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream();
-
-                int response;
-                byte[] buffer = new byte[1024];
-                while ((response = inputStream.read(buffer)) != -1) {
-                    handleServerMessage(new String(buffer, 0, response));
+        try {
+            socket = new Socket(serverIP, 41242);
+    
+            // Создать поток для чтения данных от сервера
+            new Thread(() -> {
+                try {
+                    InputStream inputStream = socket.getInputStream();
+                    int response;
+                    byte[] buffer = new byte[1024];
+                    while ((response = inputStream.read(buffer)) != -1) {
+                        handleServerMessage(new String(buffer, 0, response));
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }).start();
+            }).start();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     private void incrementCounter() {
@@ -69,50 +74,32 @@ public class Client extends Application {
     }
 
     private void sendCounterValueToServer() {
-        new Thread(() -> {
-            try {
-                // Устанавливаем соединение с сервером
-                Socket socket = new Socket(serverIP, 41242);
-
-                // Отправляем значение счетчика на сервер
-                OutputStream outputStream = socket.getOutputStream();
-                String counterValue = Integer.toString(counter);
-                outputStream.write(counterValue.getBytes());
-                outputStream.flush();
-
-                // Закрываем соединение
-                socket.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }).start();
-    }
-
-    private synchronized void handleClientConnection(Socket clientSocket) {
         try {
-            OutputStream outputStream = clientSocket.getOutputStream();
-            clientOutputStreams.add(outputStream);
-
-            new Thread(() -> {
-                try {
-                    InputStream inputStream = clientSocket.getInputStream();
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        // Обработка входящих данных, если необходимо
-                        incrementCounter();
-                        updateCounterLabel();
-                        sendCounterValueToClients();
-                    }
-                    // Закрываем соединение после завершения чтения
-                    clientSocket.close();
-                    clientOutputStreams.remove(outputStream);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Отправляем значение счетчика на сервер
+            OutputStream outputStream = socket.getOutputStream();
+            String counterValue = Integer.toString(counter);
+            outputStream.write(counterValue.getBytes());
+            outputStream.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
+    
+
+    // You might need to implement this method based on your requirements
+    private void handleServerMessage(String message) {
+        if (message.startsWith("Counter: ")) {
+            // Обработка сообщения с текущим значением счетчика (как ранее)
+            // ...
+        } else if (message.startsWith("UpdatedCounter: ")) {
+            // Обработка сообщения с обновленным значением счетчика
+            String[] parts = message.split(": ");
+            if (parts.length == 2) {
+                int updatedCounterValue = Integer.parseInt(parts[1]);
+                counter = updatedCounterValue;
+                updateCounterLabel();
+            }
+        }
+    }
+    
 }
--- src/main/java/com/example/increment/Server.java
+++ src/main/java/com/example/increment/Server.java
@@ -1,15 +1,5 @@
 package com.example.increment;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -18,11 +8,17 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class Server extends Application {
     private int counter = 0;
-    @FXML
-    private Button btn;
-    @FXML
     private Label lableid;
     private List<OutputStream> clientOutputStreams = new ArrayList<>();
 
@@ -32,12 +28,13 @@ public class Server extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        VBox root = loader.load();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Server");
-        primaryStage.show();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+    VBox root = loader.load();
+    Scene scene = new Scene(root);
+    scene.getStylesheets().add(getClass().getResource("dark_theme.css").toExternalForm());
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Server");
+    primaryStage.show();
 
         Button clickButton = (Button) loader.getNamespace().get("btn");
         clickButton.setOnAction(e -> {
@@ -72,40 +69,66 @@ public class Server extends Application {
 
     private synchronized void sendCounterValueToClients() {
         String message = "Counter: " + counter;
+        List<OutputStream> closedStreams = new ArrayList<>();
+    
         for (OutputStream clientOutputStream : clientOutputStreams) {
             try {
                 clientOutputStream.write(message.getBytes());
                 clientOutputStream.flush();
             } catch (IOException e) {
-                e.printStackTrace();
+                // Ошибка записи, возможно, сокет был закрыт клиентом
+                closedStreams.add(clientOutputStream);
+            }
+        }
+    
+        // Удалить закрытые потоки из списка
+        clientOutputStreams.removeAll(closedStreams);
+    
+        // Обновить собственный счетчик и метку на сервере
+        updateCounterLabel();
+    
+        // Отправить обновленное значение счетчика клиентам
+        String updatedCounterMessage = "UpdatedCounter: " + counter;
+        for (OutputStream clientOutputStream : clientOutputStreams) {
+            try {
+                clientOutputStream.write(updatedCounterMessage.getBytes());
+                clientOutputStream.flush();
+            } catch (IOException e) {
+                // Обработка ошибок записи, если необходимо
             }
         }
     }
+    
+    
 
     private synchronized void handleClientConnection(Socket clientSocket) {
-        try {
-            OutputStream outputStream = clientSocket.getOutputStream();
-            clientOutputStreams.add(outputStream);
+    try {
+        OutputStream outputStream = clientSocket.getOutputStream();
 
-            new Thread(() -> {
-                try {
-                    InputStream inputStream = clientSocket.getInputStream();
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        // Обработка входящих данных, если необходимо
-                        incrementCounter();
-                        updateCounterLabel();
-                        sendCounterValueToClients();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        // Добавить outputStream только один раз при создании сокета для клиента
+        clientOutputStreams.add(outputStream);
+
+        new Thread(() -> {
+            try {
+                InputStream inputStream = clientSocket.getInputStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    // Обработка входящих данных, если необходимо
+                    incrementCounter();
+                    updateCounterLabel();
+                    sendCounterValueToClients();
                 }
-            }).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            } catch (IOException e) {
+                // Ошибка чтения, возможно, сокет был закрыт клиентом
+                clientOutputStreams.remove(outputStream);
+                e.printStackTrace();
+            }
+        }).start();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
 
     private synchronized void incrementCounter() {
         counter++;
